683f848846abd55ec8bd7e294289c48f9aba2ef1
Christoph Dorn <christoph@christophdorn.com>
2015-10-17 17:55:14 -0700

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
+#!/bin/bash -e
+if [ -z "$npm_config_argv" ]; then
+	echo "ERROR: Must run with 'npm install'!"
+	exit 1
+fi
 if [ -z "$HOME" ]; then
 	echo "ERROR: 'HOME' environment variable is not set!"
 	exit 1
 fi
+
+if [ -z "$Z0_ROOT" ]; then
+	export Z0_ROOT="$__BO_DIR__/../.."
+fi
+pushd "$Z0_ROOT" > /dev/null
+	git submodule update --init --rebase lib/bash.origin
+	lib/bash.origin/bash.origin BO install
+popd > /dev/null
+
 # Source https://github.com/bash-origin/bash.origin
 . "$HOME/.bash.origin"
 function init {
@@ -10,10 +23,6 @@ function init {
 	BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
 	local __BO_DIR__="$___TMP___"
 
-
-    if [ -z "$Z0_ROOT" ]; then
-		export Z0_ROOT="$__BO_DIR__/../.."
-	fi
 
 	function Deploy {
 		BO_format "$VERBOSE" "HEADER" "Installing system ..."
